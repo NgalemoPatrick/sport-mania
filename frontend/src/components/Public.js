2e2123491790298351a311ab9f0f8f90ca236be3
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-04 17:49:44 -0500

    add background image and register file

--- frontend/src/components/Public.js
+++ frontend/src/components/Public.js
@@ -1,24 +1,49 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-
-import React from 'react'
+import React from "react";
 
 const Public = () => {
   return (
     <section className="public">
-        <header>
-            <h1>Welcome to <span className="nowrap">SPORTS MANIA</span></h1>
-        </header>
-        <main className="public_main">
-            <p>
+      <header>
+        {/* <h1>Welcome to <span className="nowrap">SPORTS MANIA</span></h1> */}
+      </header>
+      <main className="public_main">
+        {/* <p>
                 Sports Mania is a platform where you can connect and share with others pationate of sports.
-            </p>
-        </main>
-        <footer>
-            <Link to ='/login'>Click Here to login</Link>
-        </footer>
-    </section>
-  )
-}
+            </p> */}
+      </main>
+      <footer>
+        <NavLink
+          to="/login"
+          style={({ isActive }) =>
+            isActive
+              ? {
+                  color: "#fff",
+                  background: "#0A3442",
+                }
+              : { color: "#545e6f", background: "#f0f0f0" }
+          }
+        >
+          Login
+        </NavLink>
 
-export default Public
+        <NavLink
+          to="/Register"
+          style={({ isActive }) =>
+            isActive
+              ? {
+                  color: "#fff",
+                  background: "#0A3442",
+                }
+              : { color: "#545e6f", background: "#f0f0f0" }
+          }
+        >
+          Don'thave an account? Sign up
+        </NavLink>
+      </footer>
+    </section>
+  );
+};
+
+export default Public;
