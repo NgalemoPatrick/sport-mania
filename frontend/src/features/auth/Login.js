8e51f86c08b11a9ee610232b0785c55e88619816
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-04 22:19:31 -0500

    update login form

--- frontend/src/features/auth/Login.js
+++ frontend/src/features/auth/Login.js
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect, useContext } from "react";
 // import AuthContext from "./context/AuthProvider";
-
+import { Link } from "react-router-dom";
 import axios from "axios";
 const LOGIN_URL = "/auth";
 
@@ -57,63 +57,51 @@ const Login = () => {
   };
 
   return (
-    <section className="login">
+    <section className="login-form">
       <section className="login-side">
-        <>
-          {success ? (
-            <section>
-              <h1>You are logged in!</h1>
-              <br />
-              <p>
-                <a href="#">Go to Home</a>
-              </p>
-            </section>
-          ) : (
-            <section>
-              <p
-                ref={errRef}
-                className={errMsg ? "errmsg" : "offscreen"}
-                aria-live="assertive"
-              >
-                {errMsg}
-              </p>
-              <h1>Sign In</h1>
-              <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input
-                  type="text"
-                  id="username"
-                  ref={userRef}
-                  autoComplete="off"
-                  onChange={(e) => setUser(e.target.value)}
-                  value={user}
-                  required
-                />
+        <p
+          ref={errRef}
+          className={errMsg ? "errmsg" : "offscreen"}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+          />
 
-                <label htmlFor="password">Password:</label>
-                <input
-                  type="password"
-                  id="password"
-                  onChange={(e) => setPwd(e.target.value)}
-                  value={pwd}
-                  required
-                />
-                <button>Sign In</button>
-              </form>
-              <p>
-                Need an Account?
-                <br />
-                <span className="line">
-                  {/*put router link here*/}
-                  <a href="#">Sign Up</a>
-                </span>
-              </p>
-            </section>
-          )}
-        </>
-        )
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+          />
+          <button>Sign In</button>
+        </form>
+        <p>
+          Need an Account?
+          <br />
+          <span className="line">
+           
+            <Link to='/register'>Sign Up</Link>
+            
+          </span>
+        </p>
       </section>
-      <section className="login-logo_side"></section>
+          <section className="login-logo_side">
+          
+          </section>
     </section>
   );
 };
