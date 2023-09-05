85667829cf831d6f638bcfc52d4665bbbcb30e82
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-05 09:19:47 -0500

    define the statis files route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
+// initial import 
 const express = require('express')
+const app = express()
+const path = require("path")
+const PORT = process.env.PORT || 5100
+
+
+
+// define the static files route
+app.use('/', express.static(path.join(__dirname, '/public')))
+// Start the app
+app.listen(PORT, () => console.log(`Server runniing on port ${PORT}`))
