10f654ba245fa0328b7e80be65f4c83dbe0a9f08
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-05 09:29:45 -0500

    add the root route for the app

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,8 @@ const PORT = process.env.PORT || 5100
 
 // define the static files route
 app.use('/', express.static(path.join(__dirname, '/public')))
+
+// define the root route of the app
+app.use('/', require('./routes/root'))
 // Start the app
 app.listen(PORT, () => console.log(`Server runniing on port ${PORT}`))
